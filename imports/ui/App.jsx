0b91591dec84fd31cc9c29a6d65c82a7d2c7c5c8
Jon Haase <haase1020@gmail.com>
2020-08-21 20:39:18 -0400

    done with section 4 in tutorial

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -4,17 +4,31 @@ import { Task } from './Task';
 import Tasks from '/imports/api/tasks';
 import { TaskForm } from './TaskForm';
 
+const toggleChecked = ({ _id, isChecked }) => {
+  Tasks.update(_id, {
+    $set: {
+      isChecked: !isChecked,
+    },
+  });
+};
+
+const deleteTask = ({ _id }) => Tasks.remove(_id);
 export const App = () => {
   const tasks = useTracker(() =>
     Tasks.find({}, { sort: { createdAt: -1 } }).fetch()
   );
 
   return (
-    <div>
+    <div className='simple-todos-react'>
       <h1>Welcome to Meteor!</h1>
-      <ul>
+      <ul className='tasks'>
         {tasks.map((task) => (
-          <Task key={task._id} task={task} />
+          <Task
+            key={task._id}
+            task={task}
+            onCheckboxClick={toggleChecked}
+            onDeleteClick={deleteTask}
+          />
         ))}
       </ul>
       <TaskForm />
--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import classnames from 'classnames';
 
-export const Task = ({ task }) => {
-  return <li>{task.text}</li>;
+export const Task = ({ task, onCheckboxClick }) => {
+  const classes = classnames('task', {
+    checked: Boolean(task.isChecked),
+  });
+
+  return (
+    <li className={classes}>
+      <button onClick={() => onDeleteClick(task)}>&times;</button>
+      <span>{task.text}</span>
+      <input
+        type='checkbox'
+        checked={Boolean(task.isChecked)}
+        onClick={() => onCheckboxClick(task)}
+        readOnly
+      />
+      <span>{task.text}</span>
+    </li>
+  );
 };
